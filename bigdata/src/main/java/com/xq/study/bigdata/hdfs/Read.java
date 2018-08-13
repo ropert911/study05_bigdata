c7ab37a4482efa9aae8539b600cf9a6fdb9d127d
ropert911 <ropert911@163.com>
2018-08-13 19:41:28 +0800

    添加示例

---
+++ bigdata/src/main/java/com/xq/study/bigdata/hdfs/Read.java
@@ -0,0 +1,55 @@
+package com.xq.study.bigdata.hdfs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.io.IOUtils;
+
+/**
+ * Created by sk-qianxiao on 2018/8/13.
+ */
+public class Read {
+    public static void main(String[] args) {
+        try {
+            Configuration conf = new Configuration();
+            conf.set("fs.defaultFS", "hdfs://10.10.22.96:9000");
+            conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
+
+            FileSystem fs = FileSystem.get(conf);
+            Path file = new Path("/user/root/input/kms-acls.xml");
+
+            //判断文件是否存在
+            if (!fs.exists(file)) {
+                return;
+            }
+
+            FSDataInputStream getIt = fs.open(file);
+
+            BufferedReader d = new BufferedReader(new InputStreamReader(getIt));
+            String content = d.readLine(); //读取文件一行
+            while (null != content) {
+                System.out.println(content);
+                System.out.flush();
+                content = d.readLine();
+            }
+
+            System.out.println("===================用IOUtils.copyBytes读取");
+            getIt.seek(0);
+            OutputStream outputStream = new ByteOutputStream(10240);
+            IOUtils.copyBytes(getIt, outputStream, conf, false);
+            System.out.print(outputStream.toString());
+            d.close(); //关闭文件
+            fs.close(); //关闭hdfs
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
---
+++ bigdata/src/main/java/com/xq/study/bigdata/hdfs/Write.java
@@ -0,0 +1,34 @@
+package com.xq.study.bigdata.hdfs;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+/**
+ * Created by sk-qianxiao on 2018/8/13.
+ */
+public class Write {
+    public static void main(String[] args) {
+        try {
+            Configuration conf = new Configuration();
+            conf.set("fs.defaultFS", "hdfs://10.10.22.96:9000");
+            conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
+
+
+            byte[] buff = "Hello world".getBytes(); // 要写入的内容
+
+            String filename = "test"; //要写入的文件名
+            FileSystem fs = FileSystem.get(conf);
+            FSDataOutputStream os = fs.create(new Path(filename));
+            os.write(buff, 0, buff.length);
+            System.out.println("Create:" + filename);
+
+
+            os.close();
+            fs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

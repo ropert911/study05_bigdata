4e90451186232b310faf3d335d8e5ca14305e7e9
ropert911 <ropert911@163.com>
2018-08-30 15:00:25 +0800

    添加示例

---
+++ bg_scala/src/main/scala/spark/stream/Kafak_WordCount.scala
@@ -0,0 +1,33 @@
+package spark.stream
+
+import org.apache.spark._
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka._
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.streaming.kafka.KafkaUtils
+
+object Kafak_WordCount {
+  def main(args: Array[String]) {
+    StreamingExamples.setStreamingLogLevels()
+
+    val sc = new SparkConf().setAppName("KafkaWordCount").setMaster("local[2]")
+    val ssc = new StreamingContext(sc, Seconds(10))
+    //设置检查点，如果存放在HDFS上面，则写成类似ssc.checkpoint("/user/hadoop/checkpoint")这种形式，但是，要启动hadoop
+    ssc.checkpoint("file:///usr/local/spark/mycode/kafka/checkpoint")
+    val zkQuorum = "localhost:2181" //Zookeeper服务器地址
+    val group = "1" //topic所在的group，可以设置为自己想要的名称，比如不用1，而是val group = "test-consumer-group"
+    val topics = "wordsender" //topics的名称
+    val numThreads = 1 //每个topic的分区数
+    val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
+    val lineMap = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap)
+    val lines = lineMap.map(_._2)
+    val words = lines.flatMap(_.split(" "))
+    val pair = words.map(x => (x, 1))
+    val wordCounts = pair.reduceByKeyAndWindow(_ + _, _ - _, Minutes(2), Seconds(10), 2) //这行代码的含义在下一节的窗口转换操作中会有介绍
+    wordCounts.print
+
+    ssc.start
+    ssc.awaitTermination
+  }
+}
---
+++ bg_scala/src/main/scala/spark/stream/Kafka_WordProducer.scala
@@ -0,0 +1,38 @@
+package spark.stream
+
+import java.util.HashMap
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka._
+
+object Kafka_WordProducer {
+  def main(args: Array[String]) {
+    if (args.length < 4) {
+      System.err.println("Usage: KafkaWordCountProducer <metadataBrokerList> <topic> " +
+        "<messagesPerSec> <wordsPerMessage>")
+      System.exit(1)
+    }
+
+    val Array(brokers, topic, messagesPerSec, wordsPerMessage) = args
+
+    // Zookeeper connection properties
+    val props = new HashMap[String, Object]()
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers)
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+    val producer = new KafkaProducer[String, String](props)
+
+    // Send some messages
+    while (true) {
+      (1 to messagesPerSec.toInt).foreach { messageNum =>
+        val str = (1 to wordsPerMessage.toInt).map(x => scala.util.Random.nextInt(10).toString).mkString(" ")
+        print(str)
+        println()
+        val message = new ProducerRecord[String, String](topic, null, str)
+        producer.send(message)
+      }
+      Thread.sleep(1000)
+    }
+  }
+}

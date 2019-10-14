9f2d2116752b25d869e396251d02ecc8a536d345
root <254323626@qq.com>
2019-10-14 22:06:11 +0800

    代码调整

---
+++ demo1flume/src/main/java/com/xq/study/flume/demo1flume/Demo1flumeApplication.java
@@ -0,0 +1,13 @@
+package com.xq.study.flume.demo1flume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1flumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1flumeApplication.class, args);
+    }
+
+}

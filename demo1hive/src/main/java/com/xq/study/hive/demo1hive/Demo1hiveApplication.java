2f7237369bd9c4018139aacf1a60a229385a1945
ropert911 <254323626@qq.com>
2019-10-15 21:02:35 +0800

    代码调整

---
+++ demo1hive/src/main/java/com/xq/study/hive/demo1hive/Demo1hiveApplication.java
@@ -0,0 +1,13 @@
+package com.xq.study.hive.demo1hive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1hiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1hiveApplication.class, args);
+    }
+
+}
---
+++ demo1hive/src/test/java/com/xq/study/hive/demo1hive/Demo1hiveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xq.study.hive.demo1hive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demo1hiveApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

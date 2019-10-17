2b7bcb05ffe46470a341f211af18f534a02912c7
ropert911 <254323626@qq.com>
2019-10-17 16:03:09 +0800

    代码调整

--- scala_example/src/main/scala/com/study/scala/examples/wordcount.scala
+++ scala_example/src/main/scala/com/study/scala/examples/wordcount.scala
@@ -7,14 +7,21 @@ import org.apache.spark.{SparkConf, SparkContext}
   */
 object wordcount {
   def main(args: Array[String]) {
-    //如果不设置，就默认就是sk-qianxiao，没有权限的
-    System.setProperty("HADOOP_USER_NAME", "root")
-    val conf = new SparkConf().setAppName("Simple Application")
-      .setMaster("local")
-    //      .setMaster('spark://10.21.208.21:7077')
+    //hadoop用到的:如果不设置，就默认就是sk-qianxiao，没有权限的
+    System.setProperty("HADOOP_USER_NAME", "hdfs")
+    //spark用到的
+    System.setProperty("user.name", "hdfs")
+    //    val conf = new SparkConf().setAppName("Simple Application").setMaster("local").set("dfs.client.use.datanode.hostname", "true")
+    val conf = new SparkConf().setAppName("Simple Application").set("dfs.client.use.datanode.hostname", "true")
+      //      .setMaster("local")
+      //      .setMaster('spark://10.21.208.21:7077')
+//      .setMaster("mesos://host:port")
+      .setMaster("yarn-cluster")
     //    .setMaster("yarn-client")
     //使用主机名进行访问，要不然hdfs返回的就是内网ip
-    conf.set("dfs.client.use.datanode.hostname", "true")
+    //    conf.set("dfs.client.use.datanode.hostname", "true")
+    //    conf.set("dfs.datanode.use.datanode.hostname", "true")
+
 
     val sc = new SparkContext(conf)
 
@@ -31,4 +38,17 @@ object wordcount {
 
     sc.stop()
   }
+
+
+
+
+
+
+
+//  val spark = SparkSession
+//    .builder()
+//    .appName("SparkSQLApp")
+//    .master("local[2]")
+//    .config("dfs.client.use.datanode.hostname", "true")
+//    .getOrCreate()
 }

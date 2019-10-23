3116edd67e7ada0209129733a1c2e00422bfde6d
root <254323626@qq.com>
2019-10-24 07:41:58 +0800

    代码调整

--- demo_mlib/src/main/scala/com/xq/study/demo_milib/algorithm.scala
+++ demo_mlib/src/main/scala/com/xq/study/demo_milib/algorithm.scala
@@ -15,8 +15,8 @@ object algorithm {
     val spark = SparkSession.builder().appName(KMeansTest.getClass.getName).master("local[1]").getOrCreate()
     val sc = spark.sparkContext
 
-    //    summarystatisticsTest(sc)
-    relationTest(sc)
+    summarystatisticsTest(sc)
+    //    relationTest(sc)
   }
 
   /**
@@ -33,10 +33,17 @@ object algorithm {
 
     // Compute column summary statistics.
     val summary: MultivariateStatisticalSummary = Statistics.colStats(observations)
-    println(summary.mean) //每列平均值
+    println("列最大值==>" + summary.max)
+    println("列最小值==>" + summary.min)
+    println("列平均值==>" + summary.mean) //每列平均值
     //列方差  方差等于各个数据与其算术平均数的离差平方和的平均数(平均时个数要减1) 如第1列平均为3 （4+1=1）/2=3
-    println(summary.variance)
-    println(summary.numNonzeros) // 每列中的非零个数 number of nonzeros in each column
+    println("列方差==>" + summary.variance)
+    //L0范数指向量中非零元素的个数
+    println("L0范数每列中的非零个数==>" + summary.numNonzeros)
+    //L1范数：向量中每个元素绝对值的和
+    println("L1范数==>" + summary.normL1)
+    //L2范数：向量元素绝对值的平方和再开平方
+    println("L2范数==>" + summary.normL2)
   }
 
   /**
@@ -58,18 +65,18 @@ object algorithm {
       println(s"Correlation is: $correlation")
     }
 
-        {
-          val data: RDD[Vector] = sc.parallelize(
-            Seq(
-              Vectors.dense(1.0, 10.0, 100.0),
-              Vectors.dense(2.0, 20.0, 800.0))
+    {
+      val data: RDD[Vector] = sc.parallelize(
+        Seq(
+          Vectors.dense(1.0, 10.0, 100.0),
+          Vectors.dense(2.0, 20.0, 800.0))
 
-          ) // note that each Vector is a row and not a column
-          data.foreach(println)
+      ) // note that each Vector is a row and not a column
+      data.foreach(println)
 
-          // 使用Pearson方法计算相关性。输入"spearman"作为Spearman的方法。如果未指定方法，则默认使用Pearson方法。
-          val corrMatrix: Matrix = Statistics.corr(data, "pearson")
-          println(corrMatrix.toString)
-        }
+      // 使用Pearson方法计算相关性。输入"spearman"作为Spearman的方法。如果未指定方法，则默认使用Pearson方法。
+      val corrMatrix: Matrix = Statistics.corr(data, "pearson")
+      println(corrMatrix.toString)
+    }
   }
 }

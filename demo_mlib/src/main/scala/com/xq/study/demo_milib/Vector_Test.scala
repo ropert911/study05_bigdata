aa8ed79f7fff6b65d7ce2f7deb546ef59d4f1e01
ropert911 <254323626@qq.com>
2019-10-23 19:26:13 +0800

    代码调整

--- demo_mlib/src/main/scala/com/xq/study/demo_milib/Vector_Test.scala
+++ demo_mlib/src/main/scala/com/xq/study/demo_milib/Vector_Test.scala
@@ -14,19 +14,21 @@ object Vector_Test {
     val spark = SparkSession.builder().appName(KMeansTest.getClass.getName).master("local[1]").getOrCreate()
     val sc = spark.sparkContext
 
-    summarystatisticsTest(sc)
+    //    vectorTest(sc)
+    //    LabeledPointTest(sc)
+    MatrixTest(sc)
   }
 
   /**
     * Vector
     */
-  def vectorTest(): Unit = {
+  def vectorTest(sc: SparkContext): Unit = {
     //稠密向量：2，5，8
     val vd = Vectors.dense(2, 5, 8)
     println(vd(1))
     println(vd)
 
-    //稀疏向量 个数，序号，value
+    //稀疏向量 个数，序号，序号对应的value
     val vs = Vectors.sparse(4, Array(0, 1, 2, 3), Array(9, 3, 5, 7))
     println(vs(0)) //序号访问
     println(vs)
@@ -35,7 +37,7 @@ object Vector_Test {
   /**
     * LabeledPoint
     */
-  def LabeledPointTest(): Unit = {
+  def LabeledPointTest(sc: SparkContext): Unit = {
     import org.apache.spark.mllib.linalg.Vectors
     import org.apache.spark.mllib.regression.LabeledPoint
 
@@ -44,7 +46,7 @@ object Vector_Test {
     val pos = LabeledPoint(1.0, Vectors.dense(1.0, 0.0, 3.0))
     println(pos)
 
-    println("稀疏向量 LabeledPoint")
+    println("稀疏向量 LabeledPoint") //个数，序号，序号对应的value
     // Create a labeled point with a negative label and a sparse feature vector.
     val neg = LabeledPoint(0.0, Vectors.sparse(3, Array(0, 2), Array(1.0, 3.0)))
     println(neg)
@@ -59,75 +61,27 @@ object Vector_Test {
   /**
     * 矩阵的表示：使用密集矩阵和稀疏矩阵
     */
-  def MatrixTest(): Unit = {
+  def MatrixTest(sc: SparkContext): Unit = {
     println("密集矩阵，按列的顺序======")
     import org.apache.spark.mllib.linalg.{Matrices, Matrix}
     // Create a dense matrix ((1.0, 2.0), (3.0, 4.0), (5.0, 6.0))
     val dm: Matrix = Matrices.dense(3, 2, Array(1.0, 3.0, 5.0, 2.0, 4.0, 6.0))
     println(dm)
+
+
     println("稀疏矩阵======")
-    // Create a sparse matrix (
-    //    (0,0) 9.0
-    //    (2,1) 6.0
-    //    (1,1) 8.0
-    // Array(0, 1, 3)中的3是用来说明有多少个数据
-    val sm: Matrix = Matrices.sparse(3, 2, Array(0, 1, 3), Array(0, 2, 1), Array(9, 6, 8))
+    //    * {{{
+    //      *   1.0 0.0 4.0
+    //      *   0.0 3.0 5.0
+    //      *   2.0 0.0 6.0
+    //      * }}}
+    //    * is stored as `values: [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]`,
+    //    * `rowIndices=[0, 2, 1, 0, 1, 2]`, `colPointers=[0, 2, 3, 6]`.
+    //Array(0, 2, 3, 6)  新col的序号,即在新的col中，row数组对应的序号， 个数是cols+1,最后一个的值是3，指的是values的总长度
+    //Array(0, 2, 1, 0, 1, 2) 行入口, 个数和 values个数一致
+    //使用时: (0,2,1,0,1,2) 把0放到0后--0,0  1放到第2个数1后--1,1   2放到第3个数0后--0,2
+    //((0,0),2,(1,1),(0,2),1,2)
+    val sm: Matrix = Matrices.sparse(3, 3, Array(0, 2, 3, 6), Array(0, 2, 1, 0, 1, 2), Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0))
     println(sm)
-  }
-
-  /**
-    * 矩阵的统计值：列平均，列方差
-    */
-  def summarystatisticsTest(sc: SparkContext): Unit = {
-    val observations = sc.parallelize(
-      Seq(
-        Vectors.dense(1.0, 10.0, 100.0),
-        Vectors.dense(2.0, 15.0, 200.0),
-        Vectors.dense(3.0, 20.0, 300.0)
-      )
-    )
-
-    // Compute column summary statistics.
-    val summary: MultivariateStatisticalSummary = Statistics.colStats(observations)
-    println(summary.mean) //每列平均值
-    //列方差  方差等于各个数据与其算术平均数的离差平方和的平均数
-    println(summary.variance)
-    println(summary.numNonzeros) // 每列中的非零个数 number of nonzeros in each column
-  }
-
-  /**
-    * 相关性计算
-    *
-    * @param sc
-    */
-  def relationTest(sc: SparkContext): Unit = {
-    import org.apache.spark.mllib.linalg._
-    import org.apache.spark.mllib.stat.Statistics
-    import org.apache.spark.rdd.RDD
-    {
-      val seriesX: RDD[Double] = sc.parallelize(Array(1, 2, 3, 3, 5))
-      // a series must have the same number of partitions and cardinality as seriesX
-      val seriesY: RDD[Double] = sc.parallelize(Array(11, 22, 33, 33, 55))
-
-      // 使用Pearson方法计算相关性。输入"spearman"作为Spearman的方法。如果未指定方法，则默认使用Pearson方法。
-      val correlation: Double = Statistics.corr(seriesX, seriesY, "pearson")
-      println(s"Correlation is: $correlation")
-    }
-
-    {
-      val data: RDD[Vector] = sc.parallelize(
-        Seq(
-          Vectors.dense(1.0, 10.0, 100.0),
-          Vectors.dense(2.0, 20.0, 200.0),
-          Vectors.dense(3.0, 30.0, 300.0),
-          Vectors.dense(4.0, 60.0, 500.0))
-
-      ) // note that each Vector is a row and not a column
-      data.foreach(println)
-
-      // 使用Pearson方法计算相关性。输入"spearman"作为Spearman的方法。如果未指定方法，则默认使用Pearson方法。
-      val correlMatrix: Matrix = Statistics.corr(data, "pearson")
-      println(correlMatrix.toString)
-    }
   }
 }
---
+++ demo_mlib/src/main/scala/com/xq/study/demo_milib/algorithm.scala
@@ -0,0 +1,67 @@
+package com.xq.study.demo_milib
+
+import org.apache.spark.SparkContext
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.stat.{MultivariateStatisticalSummary, Statistics}
+
+/**
+  * @author sk-qianxiao
+  * @date 2019/10/23
+  */
+object algorithm {
+  /**
+    * 矩阵的统计值：列平均，列方差
+    */
+  def summarystatisticsTest(sc: SparkContext): Unit = {
+    val observations = sc.parallelize(
+      Seq(
+        Vectors.dense(1.0, 10.0, 100.0),
+        Vectors.dense(2.0, 15.0, 200.0),
+        Vectors.dense(3.0, 20.0, 300.0)
+      )
+    )
+
+    // Compute column summary statistics.
+    val summary: MultivariateStatisticalSummary = Statistics.colStats(observations)
+    println(summary.mean) //每列平均值
+    //列方差  方差等于各个数据与其算术平均数的离差平方和的平均数
+    println(summary.variance)
+    println(summary.numNonzeros) // 每列中的非零个数 number of nonzeros in each column
+  }
+
+  /**
+    * 相关性计算
+    *
+    * @param sc
+    */
+  def relationTest(sc: SparkContext): Unit = {
+    import org.apache.spark.mllib.linalg._
+    import org.apache.spark.mllib.stat.Statistics
+    import org.apache.spark.rdd.RDD
+    {
+      val seriesX: RDD[Double] = sc.parallelize(Array(1, 2, 3, 3, 5))
+      // a series must have the same number of partitions and cardinality as seriesX
+      val seriesY: RDD[Double] = sc.parallelize(Array(11, 22, 33, 33, 55))
+
+      // 使用Pearson方法计算相关性。输入"spearman"作为Spearman的方法。如果未指定方法，则默认使用Pearson方法。
+      val correlation: Double = Statistics.corr(seriesX, seriesY, "pearson")
+      println(s"Correlation is: $correlation")
+    }
+
+    {
+      val data: RDD[Vector] = sc.parallelize(
+        Seq(
+          Vectors.dense(1.0, 10.0, 100.0),
+          Vectors.dense(2.0, 20.0, 200.0),
+          Vectors.dense(3.0, 30.0, 300.0),
+          Vectors.dense(4.0, 60.0, 500.0))
+
+      ) // note that each Vector is a row and not a column
+      data.foreach(println)
+
+      // 使用Pearson方法计算相关性。输入"spearman"作为Spearman的方法。如果未指定方法，则默认使用Pearson方法。
+      val correlMatrix: Matrix = Statistics.corr(data, "pearson")
+      println(correlMatrix.toString)
+    }
+  }
+}

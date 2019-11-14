6f2d2149085a6daf614acbfba3bc7862040bfb65
ropert911 <254323626@qq.com>
2019-11-14 14:50:05 +0800

    代码调整

--- demo_mlib/src/main/scala/com/xq/study/demo_milib/Vector_LabelePoint_Matrix.scala
+++ demo_mlib/src/main/scala/com/xq/study/demo_milib/Vector_LabelePoint_Matrix.scala
@@ -4,6 +4,7 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
 import org.apache.spark.mllib.linalg.{Matrices, Matrix, Vectors}
 import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.stat.{MultivariateStatisticalSummary, Statistics}
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.sql.SparkSession
 
@@ -19,6 +20,7 @@ object Vector_LabelePoint_Matrix {
     val sc = spark.sparkContext
 
     vectorTest(sc)
+    vectorStatic(sc)
     LabeledPointTest(sc)
     MatrixTest(sc)
   }
@@ -37,6 +39,28 @@ object Vector_LabelePoint_Matrix {
     val vs = Vectors.sparse(4, Array(0, 1, 2, 3), Array(9, 3, 5, 7))
     println(vs(0)) //序号访问
     println(vs)
+  }
+
+  def vectorStatic(sc: SparkContext): Unit ={
+    println("向量统计===========================")
+    val observations = sc.parallelize(
+      Seq(
+        Vectors.dense(1.0, 10.0, 100.0),
+        Vectors.dense(4.0, 15.0, 200.0),
+        Vectors.dense(4.0, 20.0, 300.0)
+      )
+    )
+
+    // Compute column summary statistics.
+    val summary: MultivariateStatisticalSummary = Statistics.colStats(observations)
+    println("列最大值==>" + summary.max)
+    println("列最小值==>" + summary.min)
+    println("列平均值==>" + summary.mean) //每列平均值
+    //列方差  方差等于各个数据与其算术平均数的离差平方和的平均数(平均时个数要减1) 如第1列平均为3 （4+1=1）/2=3
+    println("列方差==>" + summary.variance)
+    println("L0范数：每列中的非零个数==>" + summary.numNonzeros)
+    println("L1范数：向量中每个元素绝对值的和==>" + summary.normL1)
+    println("L2范数：向量元素绝对值的平方和再开平方==>" + summary.normL2)
   }
 
   /**
